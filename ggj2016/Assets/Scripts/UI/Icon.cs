c949d855f42ac4197748966f866e26e658d46329
Dave Van Verdegem <dave.dendave@gmail.com>
2016-01-29 23:11:01 +0100

    Updated .gitignore Added icon prefab

---
+++ ggj2016/Assets/Scripts/UI/Icon.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Icon : MonoBehaviour
+{
+	[Header("Properties")]
+	[Range(0, 1)]public float _progress;
+
+	[Header("Components")]
+	private Image _image;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Initialize();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyUp(KeyCode.Space))
+			StartCoroutine(R_Fail());
+	}
+
+	void Initialize()
+	{
+		// Initialize references
+		_image = GetComponent<Image>();
+	}
+
+	void UpdateProgress(float progress)
+	{
+		_progress = progress;
+		_image.fillAmount = _progress;
+	}
+
+	// Visual feedback
+	public void Succes()
+	{
+
+	}
+
+	IEnumerator R_Fail()
+	{
+		yield return R_Flicker(.1f);
+
+		yield return new WaitForSeconds(.1f);
+
+		yield return R_Flicker(.1f);
+	}
+
+	IEnumerator R_Flicker(float duration)
+	{
+		// Hide the image
+		_image.enabled = false;
+
+		yield return new WaitForSeconds(duration);
+
+		// Show the image again
+		_image.enabled = true;
+	}
+}
