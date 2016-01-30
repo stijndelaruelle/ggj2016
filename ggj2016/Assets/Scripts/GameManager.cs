3c55001cfbaf6935c63530a3498068d6eed88633
Stijn Delaruelle <stijn.delaruelle@gmail.com>
2016-01-30 01:40:27 +0100

    Added both the bus & car.

--- ggj2016/Assets/Scripts/GameManager.cs
+++ ggj2016/Assets/Scripts/GameManager.cs
@@ -1,12 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Sjabloon;
+using System.Collections.Generic;
 
 public delegate void VoidDelegate();
 public delegate void IntDelegate(int i);
 
 public class GameManager : Singleton<GameManager>
 {
+    [SerializeField]
+    private List<Player> m_Players;
+    private int m_PlayersLeftScreen = 0;
+
     [SerializeField]
     private Clock m_Clock;
 
@@ -40,6 +45,27 @@ public class GameManager : Singleton<GameManager>
     }
 
     //Functions
+    private void Start()
+    {
+        foreach(Player player in m_Players)
+        {
+            player.LeftScreenEvent += OnPlayerLeftScreen;
+        }
+
+        StartGame();
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+
+        foreach (Player player in m_Players)
+        {
+            if (player != null)
+                player.LeftScreenEvent -= OnPlayerLeftScreen;
+        }
+    }
+
     public void StartGame()
     {
         Debug.Log("START GAME!");
@@ -61,6 +87,7 @@ public class GameManager : Singleton<GameManager>
     public void StartDay()
     {
         Debug.Log("START DAY!");
+        m_PlayersLeftScreen = 0;
 
         if (m_StartDayEvent != null)
             m_StartDayEvent();
@@ -72,5 +99,33 @@ public class GameManager : Singleton<GameManager>
 
         if (m_EndDayEvent != null)
             m_EndDayEvent();
+    }
+
+
+    private void OnPlayerLeftScreen()
+    {
+        Debug.Log("PLAYER LEFT THE SCREEN!");
+
+        ++m_PlayersLeftScreen;
+
+        //When all the players left the screen, end the day
+        if (m_PlayersLeftScreen >= m_Players.Count)
+        {
+            EndDay();
+        }
+    }
+
+    public bool AllChildrenInVehicle()
+    {
+        foreach(Player player in m_Players)
+        {
+            if (player.PlayerType == PlayerType.Child)
+            {
+                if (player.IsInVehicle == false)
+                    return false;
+            }
+        }
+
+        return true;
     }
 }
---
+++ ggj2016/Assets/Scripts/Gameplay/Bus.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bus : Vehicle
+{
+    [SerializeField]
+    private Clock m_Clock;
+
+    [SerializeField]
+    private int m_ArriveTime;
+
+    [SerializeField]
+    private int m_LeaveTime;
+
+    [SerializeField]
+    private Transform m_BusStop;
+    private bool m_ReachedBusStop = false;
+
+    protected override void Start()
+    {
+        base.Start();
+        m_Clock.ClockUpdatedEvent += OnClockUpdated;
+    }
+
+    private void OnDestroy()
+    {
+        if (m_Clock == null)
+            return;
+
+        m_Clock.ClockUpdatedEvent -= OnClockUpdated;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (m_ReachedBusStop)
+            return;
+
+        if (transform.position.x > m_BusStop.position.x)
+        {
+            m_IsDriving = false;
+            m_ReachedBusStop = true;
+        }   
+    }
+
+    private void OnClockUpdated(int secondsLeft)
+    {
+        Debug.Log("Time left: " + secondsLeft);
+        if (m_IsDriving)
+            return;
+
+        if (secondsLeft == m_ArriveTime || secondsLeft == m_LeaveTime)
+        {
+            m_IsDriving = true;
+        }
+    }
+}
