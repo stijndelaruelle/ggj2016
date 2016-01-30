5c6f4d3aba57e849366736a43baa0dc1bc2ce4b7
Dave Van Verdegem <dave.dendave@gmail.com>
2016-01-30 23:34:31 +0100

    Added small script

---
+++ ggj2016/Assets/Scripts/UI/TaskSprite.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TaskSprite : MonoBehaviour
+{
+	[Header("Properties")]
+	public Image _taskSprite;
+	public Image _taskCheck;
+}
