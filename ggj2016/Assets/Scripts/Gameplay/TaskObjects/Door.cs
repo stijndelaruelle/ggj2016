12e349b01b75104d0fded681fab83d8b8b19e718
Stijn Delaruelle <stijn.delaruelle@gmail.com>
2016-01-31 12:32:48 +0100

    Added 2 types of doors.

---
+++ ggj2016/Assets/Scripts/Gameplay/TaskObjects/Door.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer m_SpriteRenderer;
+
+    [SerializeField]
+    private Sprite m_ClosedDoor;
+
+    [SerializeField]
+    private Sprite m_OpenDoor;
+
+    private int m_NumberOfPlayersInDoor = 0;
+
+    private void Start()
+    {
+        m_SpriteRenderer.sprite = m_ClosedDoor;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        ++m_NumberOfPlayersInDoor;
+
+        if (m_NumberOfPlayersInDoor > 0)
+            m_SpriteRenderer.sprite = m_OpenDoor;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        --m_NumberOfPlayersInDoor;
+
+        if (m_NumberOfPlayersInDoor <= 0)
+            m_SpriteRenderer.sprite = m_ClosedDoor;
+    }
+}
